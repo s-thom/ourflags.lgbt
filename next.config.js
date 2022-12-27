d870e3abe8565248f85557463191cf3298fbcac6
Stuart Thomson <me@sthom.kiwi>
2022-12-28 11:52:17 +1300

    Disable Next's image optimisation

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    // In most cases, the images Next was producing were _larger_ after
+    // transcoding to lossy WebP. This is very much an edge case of this site,
+    // due to flags being large regions of flat colour.
+    // For this site, it's better to leave the images as their default.
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
