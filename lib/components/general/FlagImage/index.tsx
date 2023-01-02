f08d06edbf2959ca5f9d34f128153516bf603d94
Stuart Thomson <me@sthom.kiwi>
2023-01-02 20:41:41 +1300

    Remove unnecessary use client directive

--- lib/components/general/FlagImage/index.tsx
+++ lib/components/general/FlagImage/index.tsx
@@ -4,8 +4,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-"use client";
-
 import Image, { ImageProps } from "next/image";
 import { FLAG_ASPECT_RATIO, PNG_SIZES } from "../../../constants";
 import { FlagMeta } from "../../../types";
